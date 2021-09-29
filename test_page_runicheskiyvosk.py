996fc68d5b9d5d2f0931555ae44955f100985476
DelionA <marko-140210@yandex.ru>
2021-09-29 20:32:25 +0300

    refactoring func test_must_see_seven_fields, used @parametrize

--- test_page_runicheskiyvosk.py
+++ test_page_runicheskiyvosk.py
@@ -17,6 +17,10 @@ class Base:
 class TestRvPage(Base):
     # класс тест страницы htttp://site/runicheskiyvosk
     url = 'http://localhost:5000/runicheskiyvosk/'
+    runa = [
+        'runa_1', 'runa_2', 'runa_3', 'runa_4',
+        'runa_5', 'runa_6', 'runa_7'
+    ]
 
     # тест открытие страницы. урл соответстовует
     def test_open_page(self):
@@ -24,68 +28,28 @@ class TestRvPage(Base):
         assert self.browser.current_url == self.url,\
         "урл не соответствует ожидаемому"
 
-    def test_must_see_seven_fields(self):
-        # поле 1
-        field_1 = (By.CSS_SELECTOR, "#id_runa_1")
-        assert  self.find_element_(field_1),"поле 1 не найдено"
-        # проверить что поле 1 содержит 24 селект
-        element = self.browser.find_element_by_css_selector('#id_runa_1')
+    # поле селект присутствует
+    @pytest.mark.parametrize('runa', runa)
+    def test_must_see_seven_fields(self, runa):
+        selector = '#id_' + f"{runa}"
+        field = (By.CSS_SELECTOR, selector)
+        field_number = f"{runa}"[-1]
+        assert  self.element_is_present(field),"поле {} не найдено".format(field_number)
+
+    # поле содержит 24 значения селект
+    @pytest.mark.parametrize('runa', runa)
+    def test_total_values_select_field(self, runa):
+        selector = '#id_' + f"{runa}"
+        field = (By.CSS_SELECTOR, selector)
+        field_number = f"{runa}"[-1]
+        element = self.browser.find_element(*field)
         select = Select(element)
         spisok = len(select.options)
-        assert spisok - 1 == 24, 'выпадающий список поля 1 не совпадает'
+        assert spisok - 1 == 24, 'выпадающий список поля {} не совпадает'.format(field_number)
 
-
-        # поле 2
-        field_2 = (By.CSS_SELECTOR, "#id_runa_2")
-        assert  self.find_element_(field_2),"поле 2 не найдено"
-        # проверить что поле 2 содержит 24 селект
-        element = self.browser.find_element_by_css_selector('#id_runa_2')
-        select = Select(element)
-        spisok = len(select.options)
-        assert spisok - 1 == 24, 'выпадающий список поля 2 не совпадает'
-
-        # поле 3
-        field_3 = (By.CSS_SELECTOR, "#id_runa_3")
-        assert  self.find_element_(field_3),"поле 3 не найдено"
-        element = self.browser.find_element_by_css_selector('#id_runa_3')
-        select = Select(element)
-        spisok = len(select.options)
-        assert spisok - 1 == 24, 'выпадающий список поля 3 не совпадает'
-
-        # поле 4
-        field_4 = (By.CSS_SELECTOR, "#id_runa_4")
-        assert  self.find_element_(field_4),"поле 4 не найдено"
-        element = self.browser.find_element_by_css_selector('#id_runa_4')
-        select = Select(element)
-        spisok = len(select.options)
-        assert spisok - 1 == 24, 'выпадающий список поля 4 не совпадает'
-
-        # поле 5
-        field_5 = (By.CSS_SELECTOR, "#id_runa_5")
-        assert  self.find_element_(field_5),"поле 5 не найдено"
-        element = self.browser.find_element_by_css_selector('#id_runa_5')
-        select = Select(element)
-        spisok = len(select.options)
-        assert spisok - 1 == 24, 'выпадающий список поля 5 не совпадает'
-
-        # поле 6
-        field_6 = (By.CSS_SELECTOR, "#id_runa_6")
-        assert  self.find_element_(field_6),"поле 6 не найдено"
-        element = self.browser.find_element_by_css_selector('#id_runa_6')
-        select = Select(element)
-        spisok = len(select.options)
-        assert spisok - 1 == 24, 'выпадающий список поля 6 не совпадает'
-
-        # поле 7
-        field_7 = (By.CSS_SELECTOR, "#id_runa_7")
-        assert  self.find_element_(field_7),"поле 7 sне найдено"
-        element = self.browser.find_element_by_css_selector('#id_runa_7')
-        select = Select(element)
-        spisok = len(select.options)
-        assert spisok - 1 == 25, 'выпадающий список поля 7 не совпадает'
 
     # метод класса для проверки наличия элемента на странице
-    def find_element_(self, field):
+    def element_is_present(self, field):
         try:
             if self.browser.find_element(*field):
                 return True
