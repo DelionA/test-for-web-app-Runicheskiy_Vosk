ee353f8d4961ad66018906e35530cd173242d249
DelionA <marko-140210@yandex.ru>
2021-10-06 15:12:15 +0300

    add ID_7 test_should_see_field_error_if_not_unique_select_send_button func

--- test_page_runicheskiyvosk.py
+++ test_page_runicheskiyvosk.py
@@ -22,13 +22,23 @@ class TestRvPage(Base):
         'runa_5', 'runa_6', 'runa_7'
     ]
 
-    # тест открытие страницы. урл соответствует
+    values_select = (
+        ('uruz', 'uruz', 'thurisaz', 'ansuz', 'raido', 'kaunaz', 'gebo'),
+        ('fehu', 'fehu', 'thurisaz', 'ansuz', 'raido', 'kaunaz', 'gebo'),
+        ('fehu', 'uruz', 'fehu', 'ansuz', 'raido', 'kaunaz', 'gebo'),
+        ('fehu', 'uruz', 'thurisaz', 'fehu', 'raido', 'kaunaz', 'gebo'),
+        ('fehu', 'uruz', 'thurisaz', 'ansuz', 'fehu', 'kaunaz', 'gebo'),
+        ('fehu', 'uruz', 'thurisaz', 'ansuz', 'raido', 'fehu', 'gebo'),
+        ('fehu', 'uruz', 'thurisaz', 'ansuz', 'raido', 'kaunaz', 'fehu'),
+      )
+
+    # ID_1 открытие страницы. url соответствует
     def test_open_page(self):
         self.open_page(self.url)
         assert self.browser.current_url == self.url,\
         "урл не соответствует ожидаемому"
 
-    # тест поле селект присутствует
+    # ID_2 присутствует 7 полей выбора Рун
     @pytest.mark.parametrize('runa', runa)
     def test_must_see_seven_fields(self, runa):
         selector = '#id_' + f"{runa}"
@@ -36,7 +46,7 @@ class TestRvPage(Base):
         field_number = f"{runa}"[-1]
         assert  self.element_is_present(field),"поле {} не найдено".format(field_number)
 
-    # тест поле содержит 24 значения селект
+    # ID_3 каждое из 7 полей содержит 24 варианта выбора Рун
     @pytest.mark.parametrize('runa', runa)
     def test_total_values_select_field(self, runa):
         selector = '#id_' + f"{runa}"
@@ -47,21 +57,33 @@ class TestRvPage(Base):
         spisok = len(select.options)
         assert spisok - 1 == 24, 'выпадающий список поля {} не совпадает'.format(field_number)
 
-    # тест кнопка 'отправить' присутствует
-    def test_sould_see_button_send(self):
-        field = (By.CSS_SELECTOR, "button")
-        assert self.element_is_present(field), "кнопка 'отправить' отстутствует"
-
-    # туст поле ошибок отсутствует
+    # ID_4 Отсутствует поле с предупреждением об уникальности выбора значения Рун
     def test_field_errors_not_present(self):
         field = (By.CSS_SELECTOR, ".errorlist")
         assert not self.element_is_present(field), "поле ошибок не должно отображаться"
 
-    
-    # метод класса найти элемент и "нажать" на него
-    def push_the_button(self, field):
-        element = self.browser.find_element(*field)
-        element.click()
+    # ID_5 кнопка 'отправить' присутствует
+    def test_sould_see_button_send(self):
+        field = (By.CSS_SELECTOR, "button")
+        assert self.element_is_present(field), "кнопка 'отправить' отстутствует"
+
+    # ID_7 При неуникальных выбранных значениях Рун после нажатия
+    # кнопки 'отправить' появляется предужпреждение
+    @pytest.mark.parametrize('values_select', values_select)
+    def test_should_see_field_error_if_not_unique_select_send_button(self, values_select):
+        self.test_open_page()
+        value_1, value_2, value_3,value_4, value_5, value_6, value_7 = values_select
+        Select(self.browser.find_element(By.CSS_SELECTOR, "#id_runa_1")).select_by_value(value_1)
+        Select(self.browser.find_element(By.CSS_SELECTOR, "#id_runa_2")).select_by_value(value_2)
+        Select(self.browser.find_element(By.CSS_SELECTOR, "#id_runa_3")).select_by_value(value_3)
+        Select(self.browser.find_element(By.CSS_SELECTOR, "#id_runa_4")).select_by_value(value_4)
+        Select(self.browser.find_element(By.CSS_SELECTOR, "#id_runa_5")).select_by_value(value_5)
+        Select(self.browser.find_element(By.CSS_SELECTOR, "#id_runa_6")).select_by_value(value_6)
+        Select(self.browser.find_element(By.CSS_SELECTOR, "#id_runa_7")).select_by_value(value_7)
+        field = (By.CSS_SELECTOR, "button")
+        self.push_the_button(field)
+        field_error = (By.CSS_SELECTOR, ".errorlist")
+        assert self.element_is_present(field_error), "поле ошибок должно быть, но его нет."
 
     # метод класса для проверки наличия элемента на странице
     def element_is_present(self, field):
@@ -71,6 +93,11 @@ class TestRvPage(Base):
         except:
             return False
 
-    #метод класса для открытия страницы
+    # метод класса для открытия страницы
     def open_page(self,url):
         self.browser.get(url)
+
+    # метод класса найти элемент и "нажать" на него
+    def push_the_button(self, field):
+        element = self.browser.find_element(*field)
+        element.click()
